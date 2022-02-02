7356d3f0a6c89268dd579679b512cec51484fd34
bald-cat <babaiev.ihor@gmail.com>
2022-02-02 22:11:11 +0200

    Added method for fetUserIp

--- symfony/src/Controller/HomePageController.php
+++ symfony/src/Controller/HomePageController.php
@@ -3,17 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
+
     public function index(): Response
     {
 
-        return phpinfo();
-//        return $this->render('home_page/index.html.twig', [
-//            'controller_name' => 'HomePageController',
-//        ]);
+        return $this->render('home_page/index.html.twig', [
+            'ip' => $this->getUserIp(),
+        ]);
     }
+
+    /**
+     * Return User Ip
+     * Need for identity user and user links
+     *
+     * @return string
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    public function getUserIp(): string
+    {
+        return $this->container->get('request_stack')->getCurrentRequest()->getClientIp();
+    }
+
 }
